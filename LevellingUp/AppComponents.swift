9c5b63a3f570199cffb588b79daf0e36f106cdef
Sam Davies <sdavies@shinobicontrols.com>
2015-03-18 15:13:33 +0000

    Create a structure for the app
    This _might_ make it easy to chuck in loads of sample view controllers.
    Can't imagine it'll actually work though.

---
+++ LevellingUp/AppComponents.swift
@@ -0,0 +1,58 @@
+//
+// Copyright 2015 Scott Logic
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import UIKit
+
+enum AppComponentCategory {
+  case Fix
+  case NewFeature
+}
+
+enum AppComponentType {
+  case Storyboard(String)
+  case Code((UIStoryboard?) -> UIViewController?)
+}
+
+struct AppComponent {
+  let name: String
+  let category: AppComponentCategory
+  let type: AppComponentType
+  
+  init(name: String, category: AppComponentCategory, type: AppComponentType) {
+    self.name = name
+    self.category = category
+    self.type = type
+  }
+  
+  init(name: String, category: AppComponentCategory, vcIdentifier: String) {
+    let type = AppComponentType.Storyboard(vcIdentifier)
+    init(name: name, category: category, type: type)
+  }
+  
+  init(name: String, category: AppComponentCategory, vcInstantiation: (UIStoryboard?) -> UIViewController?) {
+    let type = AppComponentType.Code(vcInstantiation)
+    init(name: name, category: category, type: type)
+  }
+}
+
+
+
+extension AppComponent {
+  static func appStructure() -> [AppComponent] {
+    let component = AppComponent(name: "hi", category: .Fix, vcIdentifier: "DetailVC")
+    return [ component ]
+  }
+}
